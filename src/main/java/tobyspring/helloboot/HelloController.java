6f8205620ddbbfe2c047d44e7a92cf92c86d10d9
chobolevel <rodaka123@naver.com>
2023-02-07 23:30:25 +0900

    2023-02-07

---
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -0,0 +1,29 @@
+package tobyspring.helloboot;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping
+    public String hello(String name) {
+        if(name == null || name.trim().length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        return helloService.sayHello(name);
+    }
+
+}
---
+++ src/test/java/tobyspring/helloboot/HelloControllerTest.java
@@ -0,0 +1,34 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloControllerTest {
+
+    @Test
+    void helloController() {
+        HelloController helloController = new HelloController(name -> name);
+
+        String res = helloController.hello("Test");
+
+        Assertions.assertThat(res).isEqualTo("Test");
+    }
+
+    @Test
+    void failHelloController() {
+        HelloController helloController = new HelloController(new HelloService() {
+            @Override
+            public String sayHello(String name) {
+                return name;
+            }
+        });
+        Assertions.assertThatThrownBy(() -> {
+            helloController.hello(null);
+        }).isInstanceOf(IllegalArgumentException.class);
+
+        Assertions.assertThatThrownBy(() -> {
+            helloController.hello("");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
